10e15da9a4ad5024d9d65e731975c112d39c10c5
Dinesh Bala <baladnes@gmail.com>
2021-05-17 21:06:26 +0545

    Established DB connection with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({});
 
 const app = express();
+
+// DATABASE Connection
+mongoose
+  .connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => console.log("Connected to DB successfully"))
+  .catch((err) => console.log(err));
 
 // Listening to SERVER
 const PORT = process.env.PORT || 3000;
